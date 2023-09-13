56e511899e7d094097d9e2386022e5b8c88dbebb
Matthew McDonnell <mcdonnell.matthew@ymail.com>
2023-09-13 16:26:22 +0700

    adding gunicorn per chatgtp  changed port to 8080

---
+++ gunicorn.conf.py
@@ -0,0 +1,5 @@
+bind = "0.0.0.0:8080"
+
+bind = "0.0.0.0:8080"
+workers = 4  # Adjust the number of workers as needed
+app_name = "app"  # Replace "your_module_name" with the actual name of your Python module
